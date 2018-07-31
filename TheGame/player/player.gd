4f39a850e98210e7f460de4a51d7d561dbe3baa6
roxydao97 <roxydao97@gmail.com>
2018-07-31 14:56:46 +0300

    prize spawning effect

--- TheGame/player/player.gd
+++ TheGame/player/player.gd
@@ -33,6 +33,7 @@ func _ready():
 	
 func appear(anim): #appear when added to area
 	show()
+	$AnimationPlayer.play("spawning")
 	playerMovable = true
 	
 func _input(event):
@@ -145,6 +146,7 @@ func updateHP(newHP):
 	
 #called when level up
 func levelup():
+	$AnimationPlayer.play("spawning")
 	HP = maxHP * 3/2
 	DEF = DEF * 3/2
 	DAME = DAME + 10
